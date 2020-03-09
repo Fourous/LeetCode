93e78a3dea14bc4570c8364c17bc25e86bddb2e7
fourous <fourousky@gmail.com>
2020-03-09 18:45:22 +0800

    add 7and8

--- huawei/oj_8.java
+++ huawei/oj_8.java
@@ -1,26 +1,27 @@
 import java.util.*;
+
 /**
-* @author fourous
-* @date: 2020/3/9
-* @description:
- * 数据表记录包含表索引和数值（int范围的整数），请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照key值升序进行输出。
- *
+ * @author fourous
+ * @date: 2020/3/9
+ * @description: 数据表记录包含表索引和数值（int范围的整数），请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照key值升序进行输出。
  * 这题有个陷阱，就是输出要升序排列，很小的一个细节问题，如果这里是HashMap要排序才能输出，直接TreeMap会省事
- * 编译器只要是JDK-8以上，用Lambda
-*/
+ * TreeMap SortMap接口，基于红黑树,默认按键的升序排序,键、值都不能为null，效率较低
+ * HashMAP 基于哈希散列表实现,随机存储，HashMap key value 都能为Null，效率较高
+ * 编译器只要是JDK-8以上，用Lambda比较简洁
+ */
 public class oj_8 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        while (scan.hasNext()){
+        while (scan.hasNext()) {
             int number = scan.nextInt();
-            Map<Integer,Integer> map = new TreeMap<>();
-            for(int i=0;i<number;i++){
+            Map<Integer, Integer> map = new TreeMap<>();
+            for (int i = 0; i < number; i++) {
                 int key = scan.nextInt();
                 int value = scan.nextInt();
-                map.merge(key,value,(oldVal,newVal)->oldVal+newVal);
+                map.merge(key, value, (oldVal, newVal) -> oldVal + newVal);
             }
-            for(Integer key:map.keySet()){
-                System.out.println(key+" "+map.get(key));
+            for (Integer key : map.keySet()) {
+                System.out.println(key + " " + map.get(key));
             }
         }
     }
